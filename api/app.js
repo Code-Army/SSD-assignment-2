715d1d4d7b6109d9c528740db09c9ca23810dd05
D.D.M.S.M.Dissanayake <madushanidissanayake1998@gmail.com>
2021-10-03 21:58:05 +0530

    update upload

--- api/app.js
+++ api/app.js
@@ -1,14 +1,19 @@
 const fs = require("fs");
 const express = require("express");
 const multer = require("multer");
+
+//set google api credentials file
 const OAuth2Data = require("../config/credentials.json");
-var name,pic
+//add calender API serives
+const googleCalenderService =require('../services/google-calendar.service');
+var name,pic,events_list = []
 
 const { google } = require("googleapis");
 
 const app = express();
+const data= {}
 
-
+//set credentials
 const CLIENT_ID = OAuth2Data.web.client_id;
 const CLIENT_SECRET = OAuth2Data.web.client_secret;
 const REDIRECT_URL = OAuth2Data.web.redirect_uris[0];
@@ -18,16 +23,22 @@ const oAuth2Client = new google.auth.OAuth2(
     CLIENT_SECRET,
     REDIRECT_URL
 );
+
 var authed = false;
 
-// If modifying these scopes, delete token.json.
+// Required scopes,
 const SCOPES =
-    "https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/userinfo.profile";
+    "https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/userinfo.profile";
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+//route for getting the access token
 app.get("/google/callback", function (req, res) {
   const code = req.query.code;
+  //if the authorization code is available get the access token
   if (code) {
-    // Get an access token based on our OAuth code
+
     oAuth2Client.getToken(code, function (err, tokens) {
       if (err) {
         console.log("Error authenticating");
@@ -36,8 +47,6 @@ app.get("/google/callback", function (req, res) {
         console.log("Successfully authenticated");
         console.log(tokens)
         oAuth2Client.setCredentials(tokens);
-
-
         authed = true;
         res.redirect("/");
       }
@@ -60,6 +69,7 @@ app.get("/", (req, res) => {
       auth: oAuth2Client,
       version: "v2",
     });
+	//Get User Info
     oauth2.userinfo.get(function (err, response) {
       if (err) {
         console.log(err);
@@ -67,15 +77,31 @@ app.get("/", (req, res) => {
         console.log(response.data);
         name = response.data.name
         pic = response.data.picture
-        res.render("success", {
-          name: response.data.name,
-          pic: response.data.picture,
-          success:false
-        });
+        
+        //Get calender event
+        googleCalenderService.listEvents(oAuth2Client, (events) => {  
+          console.log(events);
+          events_list = events         
+          
+      });
+      
+        //Pass variable to client side
+      if(events_list.length === 0 ){
+        res.render("success", {name: response.data.name,pic: response.data.picture,success:false,lists:false,events:events_list});
+      }else{
+        res.render("success", {name: response.data.name,pic: response.data.picture,success:false,lists:true,events:events_list});
+      }
+        
+
+       
       }
     });
+
+
+
   }
 });
+
 
 
 app.post("/upload", (req, res) => {
@@ -94,32 +120,18 @@ app.post("/upload", (req, res) => {
         body: fs.createReadStream(req.file.path),
       };
       drive.files.create(
-          {
-            resource: fileMetadata,
-            media: media,
-            fields: "id",
-          },
-          (err, file) => {
-            if (err) {
-              // Handle error
-              console.error(err);
-            } else {
-              fs.unlinkSync(req.file.path)
-              res.render("success",{name:name,pic:pic,success:true})
-            }
-
-          }
       );
     }
   });
 });
-'
+
+
+
+
+
 
 
 
 app.listen(5000, () => {
   console.log("App is listening on Port 5000");
 });
-
-
-
